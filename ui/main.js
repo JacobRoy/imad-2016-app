af11e60c1cb18b1f0654e0018ea901d10ace97dd
JacobRoy <jacobroy8597@gmail.com>
2016-10-05 22:09:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,11 @@
 console.log('Loaded!');
 
-var element=document.getElementById('main-text');
+var element = document.getElementById('main-text'
+);
 elment.innerHTML = 'New value';
+
+//Move the image
+var img=document.getElementById('img');
+img.onclick =function(){
+    img.style.marginLeft = '100px';
+};
